b7c720e3a04eb2661aed64685bbb84990956a39b
Oyvind Fredstie <oyvind.fredstie@bouvet.no>
2026-04-28 12:32:55 +0200

    refactor: Add padding around upload button

--- src/components/FileUploader.tsx
+++ src/components/FileUploader.tsx
@@ -1,38 +1,29 @@
-import UploadFileIcon from '@mui/icons-material/UploadFile';
-import { Button, Stack, Typography } from '@mui/material';
+import UploadFileIcon from "@mui/icons-material/UploadFile";
+import { Button, Stack, Typography } from "@mui/material";
 
 type Props = {
-  onLoad: (schema: string) => void;
+    onLoad: (schema: string) => void;
 };
 
 export function FileUploader({ onLoad }: Props) {
-  const handleFile = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-    if (!file) return;
+    const handleFile = async (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
+        if (!file) return;
 
-    const text = await file.text();
-    onLoad(text);
-  };
+        const text = await file.text();
+        onLoad(text);
+    };
 
-  return (
-    <Stack direction="row" spacing={2}>
-      <Button
-        variant="contained"
-        component="label"
-        startIcon={<UploadFileIcon />}
-      >
-        Upload Schema
-        <input
-          type="file"
-          hidden
-          accept=".graphql,.gql"
-          onChange={handleFile}
-        />
-      </Button>
+    return (
+        <Stack direction="row" spacing={2} style={{ padding: 10 }}>
+            <Button variant="contained" component="label" startIcon={<UploadFileIcon />}>
+                Upload Schema
+                <input type="file" hidden accept=".graphql,.gql" onChange={handleFile} />
+            </Button>
 
-      <Typography variant="body2" color="text.secondary">
-        Upload a .graphql schema file
-      </Typography>
-    </Stack>
-  );
+            <Typography variant="body2" color="text.secondary">
+                Upload a .graphql schema file
+            </Typography>
+        </Stack>
+    );
 }
